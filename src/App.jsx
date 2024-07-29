4c04c9a896a01cbc61e39499bfcbf08c5a7b033a
Dipu Sah <sahd7929@gmail.com>
2024-07-30 05:19:30 +0530

    link created and website path created

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   
   return (
     <>
-    <h1 className="text-center font-extralight text-cyan-500">Hello My name is Dipu</h1>
+    <h1 >Hello My name is Dipu</h1>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from "react-router-dom"
+
+import Header from './components/Header.jsx'
+import Home from './components/Home.jsx'
+import Skills from './components/Skills.jsx'
+import Projects from './components/Projects.jsx'
+import Contact from './components/Contact.jsx'
+import Error from './components/Error.jsx'
+
+const route = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Header/>} errorElement={<Error/>}>
+    <Route index element={<Home/>} />
+    <Route path="skills" element={<Skills/>} />
+    <Route path="projects" element={<Projects/>} />
+    <Route path='contact' element={<Contact/>}/>
+    
+  </Route>
+))
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={route}/>
   </React.StrictMode>,
 )

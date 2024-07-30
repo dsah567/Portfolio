4e123402c29efbda1b1c600bd1ca03e82daaaec5
Dipu Sah <sahd7929@gmail.com>
2024-07-30 07:58:24 +0530

    header complete with link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div><h1>Contact</h1></div>
+    <div>
+      <h1>Contact</h1>
+      <a href="mailto:sahd7929@gmail.com">Send Mail :</a> <>sahd7929@gmail.com</>
+      <a href="tel:+917892957459">Call Now. :</a> <> +91 7892957459</>
+      </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,60 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet,NavLink } from 'react-router-dom'
+
+import LinkedinLogo from './assets/linkedin.svg'
+import GithubLogo from './assets/github.svg'
 
 export default function Header() {
   return (
   <>
-    <a href="mailto:sahd7929@gmail.com">Send Mail :</a> <>sahd7929@gmail.com</>
-    <a href="tel:+917892957459">Call Now. :</a> <> +91 7892957459</>
-    
-    <br />
-    <Link to="/">Home</Link>
-    <Link to="skills">Skills</Link>
-    <Link to="projects">Projects</Link>
-    <Link to="contact">contact</Link>
-    <br/>
+    <header className="bg-gray-800 text-white p-4">
+      <nav className="container mx-auto flex justify-between items-center">
+        <div className="flex-grow lg:flex lg:justify-center space-x-4 lg:space-x-8">
 
-    <Link to="https://www.linkedin.com/in/sahd7929/" target="_blank">Linkedin</Link>
-    <Link to="https://github.com/dsah567/" target="_blank">Github</Link>
-    <div>Header</div>
+        <NavLink 
+            to="/" 
+            className={({ isActive }) =>`${ isActive ?  "text-blue-400" : "text-white"}  hover:text-blue-600 `}
+          >
+             Home
+        </NavLink>
+
+        <NavLink 
+            to="skills"
+            className={({ isActive }) =>`${ isActive ?  "text-blue-400" : "text-white"}  hover:text-blue-600 `}
+          >
+             Skills
+        </NavLink>    
+
+        <NavLink 
+            to="projects"
+            className={({ isActive }) =>`${ isActive ?  "text-blue-400" : "text-white"}  hover:text-blue-600 `}
+          >
+            Projects
+        </NavLink>
+
+        <NavLink 
+            to="contact" 
+            className={({ isActive }) =>`${ isActive ?  "text-blue-400" : "text-white"}  hover:text-blue-600 `}
+          >
+            contact
+        </NavLink>
+
+      </div>
+
+      <div className="flex space-x-4 lg:space-x-8 ml-auto">
+
+        <Link to="https://www.linkedin.com/in/sahd7929/" target="_blank">
+          <img src={LinkedinLogo} alt='Linkedin Logo'  width={30}/>
+          </Link>
+
+        <Link to="https://github.com/dsah567/" target="_blank">
+          <img src={GithubLogo} alt='Github Logo' width={30} className='bg-slate-50 p-0.5'/>
+        </Link>
+
+      </div>
+      </nav>
+    </header>
+
     <Outlet/>
   </>
   )

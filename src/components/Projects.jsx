82d5802bbf5ed3b4847283fd070ab8ab2de20992
Dipu Sah <sahd7929@gmail.com>
2024-07-30 11:05:58 +0530

    project and skill page complete

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+   <div>
+    <section className="bg-gray-800 text-white p-4 lg:p-8">
+      <div className="container mx-auto lg:w-2/3">
+        <h2 className="text-2xl lg:text-4xl font-bold mb-4 lg:mb-8">About Portfolio Tools</h2>
+        <br />
+        <p className="mb-2 lg:mb-4 text-base lg:text-lg">
+        <Link to="https://github.com/dsah567/Portfolio" target="_blank">
+         <u> Frontend</u>
+        </Link> <br />
+        <br />
+          
+         <p>I have developed a portfolio using React, leveraging the power of React Router DOM for efficient routing and Tailwind CSS for styling and ensuring a responsive design. React, a robust JavaScript library, allows for a component-based architecture, which makes the code reusable and maintainable while improving development efficiency and rendering speed. React Router DOM facilitates seamless navigation within the application, creating a dynamic and interactive user experience. Tailwind CSS, a utility-first CSS framework, accelerates the styling process with a comprehensive set of pre-defined classes, ensuring a consistent and visually appealing design. Additionally, Tailwind CSS makes it easier to implement responsive design, ensuring the portfolio is accessible and functional across various dev</p>
+     
+        </p>
+
+        
+        <h2 className="text-2xl lg:text-4xl font-bold mb-4 lg:mb-8">About my Timetable Generating System</h2>
+        <br />
+        <p className="mb-2 lg:mb-4 text-base lg:text-lg">
+        
+          
+        <Link to="https://github.com/dsah567/Timetable-Generating-Frontend" target="_blank">
+         <u> Frontend</u>
+        </Link> <br />
+        <Link to="https://github.com/dsah567/Timetable-Generating-Backend" target="_blank">
+          <u>Backend</u>
+        </Link>
+        <br />
+        <br />
+        
+          
+         <p>The Timetable Generating System project is designed with a clear separation between frontend and backend components, each contributing to the overall functionality of the system. The frontend provides an intuitive user interface where users can input their preferences and constraints for the timetable. It is crafted to be user-friendly and responsive, ensuring a seamless experience across various devices.
+            The backend, on the other hand, handles the complex processing required to generate optimal timetables. It utilizes advanced genetic algorithms to solve the intricate problem of scheduling, which involves balancing numerous variables and constraints. Genetic algorithms are well-suited for this task due to their ability to efficiently explore large solution spaces and find near-optimal solutions. The backend processes the user inputs, applies the genetic algorithm to generate feasible schedules, and outputs the results to the frontend for user review.
+            Overall, the project integrates sophisticated algorithmic techniques with a user-centric interface, providing a powerful tool for timetable generation that can adapt to various scheduling needs and constraints.</p>
+        </p>
+      </div>
+    </section>
+
+   </div>
   )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,11 +3,47 @@ import React from 'react'
 export default function Skills() {
   return (
     <>
-    <a 
+    
+
+<section className="bg-gray-50 text-gray-800 p-4 lg:p-8">
+      <div className="container mx-auto lg:w-2/3">
+      <a 
           href="https://drive.google.com/file/d/1PS5mFCZbG1EYWUiE1sB8trhZnkmB4HMx/view?usp=sharing" 
-          className="text-blue-400 hover:text-blue-600 font-bold "
+          className="text-blue-400 hover:text-blue-600 text-2xl lg:text-4xl font-bold mb-4 lg:mb-8"
           target='_blank'
         >Download Resume</a>
+        <br />
+        <br />
+        <h2 className="text-2xl lg:text-4xl font-bold mb-4 lg:mb-8">Skills and Technologies</h2>
+        
+        <h3 className="text-xl lg:text-2xl font-semibold mb-2">JavaScript and MERN Stack</h3>
+        <ul className="list-disc pl-5 mb-4">
+          <li><strong>Frontend:</strong> HTML, CSS, Tailwind CSS, JavaScript (ReactJS)</li>
+          <li><strong>Backend:</strong> Node.js, Express.js, Mongoose</li>
+          <li><strong>Database:</strong> MongoDB</li>
+        </ul>
+
+        <h3 className="text-xl lg:text-2xl font-semibold mb-2">Java, Spring, and Spring Boot</h3>
+        <ul className="list-disc pl-5 mb-4">
+          <li><strong>Core Java:</strong> JDBC, Servlets, JSP, Hibernate, JPA</li>
+          <li><strong>Backend:</strong> Spring Framework, Spring Boot</li>
+          <li><strong>Database:</strong> SQL</li>
+        </ul>
+
+        <h3 className="text-xl lg:text-2xl font-semibold mb-2">Mobile App Development</h3>
+        <ul className="list-disc pl-5 mb-4">
+          <li><strong>React Native</strong></li>
+        </ul>
+
+        <h3 className="text-xl lg:text-2xl font-semibold mb-2">Technologies</h3>
+        <ul className="list-disc pl-5 mb-4">
+          <li><strong>Languages:</strong> C, Python, PHP, AJAX</li>
+          <li><strong>Software:</strong> Visual Studio Code, Eclipse</li>
+          <li><strong>Version Control:</strong> Git, GitHub</li>
+          <li><strong>API Development & Testing:</strong> Postman</li>
+        </ul>
+      </div>
+    </section>
     </>
   )
 }
